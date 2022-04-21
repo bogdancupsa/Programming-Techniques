0143526e443479c4fa3732499a49f03b72b44973
Razvan Ratoi <79528560+razvanratoi@users.noreply.github.com>
2022-04-21 19:21:15 +0300

    Add files via upload

--- Assignment3/src/main/java/model/Order.java
+++ Assignment3/src/main/java/model/Order.java
@@ -1,4 +1,49 @@
 package model;
 
 public class Order {
+    private int clientId;
+    private String productName;
+    private double price;
+    private int quantity;
+    private double total;
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
--- Assignment3/src/main/java/model/Product.java
+++ Assignment3/src/main/java/model/Product.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Product {
+    private int Id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }

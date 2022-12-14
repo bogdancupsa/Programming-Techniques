a59026ab264ad479d4358187383c3642ee7693e0
Razvan Ratoi <79528560+razvanratoi@users.noreply.github.com>
2022-12-14 22:13:53 +0200

    Delete src directory

--- src/main/java/business/Subject.java
+++
@@ -1,6 +0,0 @@
-package business;
-
-public interface Subject {
-    public void notifyObserver(Order order, MenuItem item);
-    public void register(presentation.Observer o);
-}
--- src/main/java/presentation/Observer.java
+++
@@ -1,9 +0,0 @@
-package presentation;
-
-import business.MenuItem;
-import business.Order;
-
-public interface Observer {
-
-    public void update(Order order, MenuItem item);
-}
